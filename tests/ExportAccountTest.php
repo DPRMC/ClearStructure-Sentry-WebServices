3740378e0125ce037a4cd8ad4746c99f8f0fc6f2
Michael Drennen <michaeldrennen74@gmail.com>
2018-08-17 12:47:11 -0600

    Messing with the env for testing.

--- tests/ExportAccountTest.php
+++ tests/ExportAccountTest.php
@@ -3,28 +3,27 @@
 use PHPUnit\Framework\TestCase;
 use DPRMC\ClearStructure\Sentry\Services\ExportAccount;
 use DPRMC\ClearStructure\Sentry\Services\Exceptions\AccountNotFoundException;
-use Dotenv\Dotenv;
 
 class ExportAccountTest extends TestCase {
 
-    public function __construct(?string $name = NULL, array $data = [], string $dataName = '') {
-        parent::__construct($name, $data, $dataName);
-        $dotenv = new Dotenv(__DIR__);
-        $dotenv->load();
-    }
-
     public function testValidExportRequest() {
-        $service  = new ExportAccount(getenv('UAT_LOCATION'), getenv('USER'), getenv('PASS'), TRUE, getenv('ACCOUNT'));
+        $service  = new ExportAccount(
+            getenv('UAT_LOCATION'),
+            getenv('USER'),
+            getenv('PASS'), TRUE,
+            getenv('ACCOUNT'));
         $response = $service->run();
         print_r($response);
         $this->assertEquals($response, TRUE);
     }
 
     public function testExportAccountThatDoesNotExist() {
-        require( '.env.php' );
         $this->expectException(AccountNotFoundException::class);
         $accountNumber = "This account does not exist";
-        $service       = new ExportAccount(getenv('UAT_LOCATION'), getenv('USER'), getenv('PASS'), TRUE, $accountNumber);
+        $service       = new ExportAccount(
+            getenv('UAT_LOCATION'),
+            getenv('USER'),
+            getenv('PASS'), TRUE, $accountNumber);
         $service->run();
     }
 
--- tests/RetrieveDataCubeOutputWithDefaultsAsExcelTest.php
+++ tests/RetrieveDataCubeOutputWithDefaultsAsExcelTest.php
@@ -1,21 +1,14 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
 use DPRMC\ClearStructure\Sentry\Services\RetrieveDataCubeOutputWithDefaultsAsExcel;
 use org\bovigo\vfs\vfsStream;
-use Dotenv\Dotenv;
 
 /**
  * Class RetrieveDataCubeOutputWithDefaultsAsExcelTest
  * phpunit --filter RetrieveDataCubeOutputWithDefaultsAsExcelTest
  */
 class RetrieveDataCubeOutputWithDefaultsAsExcelTest extends TestCase {
-
-    public function __construct(?string $name = NULL, array $data = [], string $dataName = '') {
-        parent::__construct($name, $data, $dataName);
-        $dotenv = new Dotenv(__DIR__);
-        $dotenv->load();
-    }
-
     public function testValidDataCubeRequest() {
         $downloadFilePath = vfsStream::url('downloads/excel.xls');
         $service          = new RetrieveDataCubeOutputWithDefaultsAsExcel(getenv('UAT_LOCATION'), getenv('USER'), getenv('PASS'), getenv('REPORT_NAME'));
