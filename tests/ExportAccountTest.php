0bae8fee06a258ab7b2cc06c9d6465b542dc492e
Michael Drennen <michaeldrennen74@gmail.com>
2018-08-17 12:00:13 -0600

    Added support for travis ci

--- tests/ExportAccountTest.php
+++ tests/ExportAccountTest.php
@@ -1,46 +1,30 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
 use DPRMC\ClearStructure\Sentry\Services\ExportAccount;
-use DPRMC\ClearStructure\Sentry\Services\AccountNotFoundException;
+use DPRMC\ClearStructure\Sentry\Services\Exceptions\AccountNotFoundException;
+use Dotenv\Dotenv;
 
 class ExportAccountTest extends TestCase {
 
-    public function testValidExportRequest() {
-        require('.env.php');
-
-        try{
-            $service = new ExportAccount($location,$user,$pass,true,'SBF');
-            $response = $service->run();
-            print_r($response);
-            $this->assertEquals($response, true);
-        } catch (SoapFault $e){
-            echo "\n\nSOAP FAULT\n\n" . get_class($e);
-            echo "\n" . $e->getFile() . ':' . $e->getLine();
-
-            echo "\n\n\n" . $e->getMessage() . "\n\n\n";
-            echo "\n" . $e->getTraceAsString();
-
-            echo "\n\nREQUEST:\n" . $service->getLastRequest() . "\n\n\n";
-
-            echo "\n\nRESPONSE\n" . $service->getLastResponse() . "\n\n\n";
-
-            echo "RESPONSE HEADERS:\n" . $service->getLastRequestHeaders() . "\n\n\n";
-
-
-
-        } catch(Exception $e) {
-            echo "\n\n\n\n" . get_class($e);
-            echo "\n\n\n" . $e->getMessage() . "\n\n\n";
-        }
-
-
+    public function __construct(?string $name = NULL, array $data = [], string $dataName = '') {
+        parent::__construct($name, $data, $dataName);
+        $dotenv = new Dotenv(__DIR__);
+        $dotenv->load();
     }
 
-    public function testExportAccountThatDoesNotExist(){
-        require('.env.php');
+    public function testValidExportRequest() {
+        $service  = new ExportAccount(getenv('UAT_LOCATION'), getenv('USER'), getenv('PASS'), TRUE, getenv('ACCOUNT'));
+        $response = $service->run();
+        print_r($response);
+        $this->assertEquals($response, TRUE);
+    }
+
+    public function testExportAccountThatDoesNotExist() {
+        require( '.env.php' );
         $this->expectException(AccountNotFoundException::class);
         $accountNumber = "This account does not exist";
-        $service = new ExportAccount($location,$user,$pass,true,$accountNumber);
+        $service       = new ExportAccount(getenv('UAT_LOCATION'), getenv('USER'), getenv('PASS'), TRUE, $accountNumber);
         $service->run();
     }
 
--- tests/RetrieveDataCubeOutputWithDefaultsAsExcelTest.php
+++ tests/RetrieveDataCubeOutputWithDefaultsAsExcelTest.php
@@ -2,47 +2,25 @@
 use PHPUnit\Framework\TestCase;
 use DPRMC\ClearStructure\Sentry\Services\RetrieveDataCubeOutputWithDefaultsAsExcel;
 use org\bovigo\vfs\vfsStream;
+use Dotenv\Dotenv;
+
 /**
  * Class RetrieveDataCubeOutputWithDefaultsAsExcelTest
  * phpunit --filter RetrieveDataCubeOutputWithDefaultsAsExcelTest
  */
 class RetrieveDataCubeOutputWithDefaultsAsExcelTest extends TestCase {
 
-    public function testValidDataCubeRequest() {
-    require('.env.php');
-
-    $downloadFilePath = vfsStream::url('downloads/excel.xls');
-
-    try{
-        $service = new RetrieveDataCubeOutputWithDefaultsAsExcel($location,$user,$pass,true,'Drennen - NAV');
-        $response = $service->run();
-
-        file_put_contents($downloadFilePath,$response->RetrieveDataCubeOutputWithDefaultsAsExcelResult);
-
-        //var_dump($response);
-        $this->assertFileExists($downloadFilePath);
-    } catch (SoapFault $e){
-        echo "\n\nSOAP FAULT\n\n" . get_class($e);
-        echo "\n" . $e->getFile() . ':' . $e->getLine();
-
-        echo "\n\n\n" . $e->getMessage() . "\n\n\n";
-        echo "\n" . $e->getTraceAsString();
-
-        echo "\n\nREQUEST:\n" . $service->getLastRequest() . "\n\n\n";
-
-        echo "\n\nRESPONSE\n" . $service->getLastResponse() . "\n\n\n";
-
-        echo "RESPONSE HEADERS:\n" . $service->getLastRequestHeaders() . "\n\n\n";
-
-
-
-    } catch(Exception $e) {
-        echo "\n\n\n\n" . get_class($e);
-        echo "\n\n\n" . $e->getMessage() . "\n\n\n";
+    public function __construct(?string $name = NULL, array $data = [], string $dataName = '') {
+        parent::__construct($name, $data, $dataName);
+        $dotenv = new Dotenv(__DIR__);
+        $dotenv->load();
     }
 
-
-}
-
-
+    public function testValidDataCubeRequest() {
+        $downloadFilePath = vfsStream::url('downloads/excel.xls');
+        $service          = new RetrieveDataCubeOutputWithDefaultsAsExcel(getenv('UAT_LOCATION'), getenv('USER'), getenv('PASS'), getenv('REPORT_NAME'));
+        $response         = $service->run();
+        file_put_contents($downloadFilePath, $response->RetrieveDataCubeOutputWithDefaultsAsExcelResult);
+        $this->assertFileExists($downloadFilePath);
+    }
 }
